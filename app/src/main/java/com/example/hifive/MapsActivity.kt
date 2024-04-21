a934eeeddab55ff9ac93734d7aba8d1bfff5c9ae
jtjohnnyx <jtjohnnyx@gmail.com>
2024-04-21 18:46:39 -0400

    now correctly sets the event marker's location name

--- app/src/main/java/com/example/hifive/MapsActivity.kt
+++ app/src/main/java/com/example/hifive/MapsActivity.kt
@@ -4,7 +4,10 @@ import android.R.attr.text
 import android.content.Intent
 import android.location.Address
 import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hifive.Post.PostActivity
 import com.example.hifive.databinding.ActivityMapsBinding
@@ -15,6 +18,8 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
+import java.util.Locale
 import kotlin.properties.Delegates
 
 
@@ -57,20 +62,89 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         //mMap.animateCamera(CameraUpdateFactory.zoomTo(9.0f))
+        //var llng = LatLng(40.7614, -73.9776)
+        //getAddress(llng)
         mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(42.3601, -71.0589)))
 
-        googleMap.setOnMapClickListener { latLng ->
-            //implement some geocoding
-            //val geocoder = Geocoder(this)
-            //val addresses: MutableList<Address>? = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
-
+        googleMap.setOnMapClickListener { llng ->
             // Add a marker at the tapped location
             marker?.remove()
-            marker = googleMap.addMarker(MarkerOptions().position(latLng).title("Event")) //placeholder
-            addr = "Los Angeles" //placeholder
-            latlong = "${latLng.latitude},${latLng.longitude}"
+            marker = googleMap.addMarker(MarkerOptions().position(llng).title("Event")) //placeholder
+            //addr = "Los Angeles" //placeholder
+            addr = if (getAddress(llng) != null)
+                getAddress(llng).toString()
+            else
+                ""
+            latlong = "${llng.latitude},${llng.longitude}"
         }
 
+    }
+
+    private fun getAddress(llng: LatLng) : String? {
+
+        val geocoder = Geocoder(this, Locale.getDefault())
+        var address : String? = ""
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//            geocoder.getFromLocation(
+//                llng.latitude,
+//                llng.longitude,
+//                1
+//            ) {
+//                if (it.isNotEmpty()) {
+//                    address = it[0]?.getAddressLine(0)
+//                    // Use the obtained address
+//                    Toast.makeText(this, address, Toast.LENGTH_SHORT).show()
+//                } else {
+//                    // No address found
+//                    Toast.makeText(this, "No address found for the location", Toast.LENGTH_SHORT)
+//                        .show()
+//                }
+//            }
+//        } else {
+//            try {
+//                val addresses = geocoder.getFromLocation(
+//                    llng.latitude,
+//                    llng.longitude,
+//                    1
+//                )
+//                if (addresses != null) {
+//                    if (addresses.isNotEmpty()) {
+//                        address = addresses[0]?.getAddressLine(0)
+//                        // Use the obtained address
+//                        Toast.makeText(this, address, Toast.LENGTH_SHORT).show()
+//                    } else {
+//                        // No address found
+//                        Toast.makeText(this, "No address found for the location", Toast.LENGTH_SHORT).show()
+//                    }
+//                }
+//            } catch (e: IOException) {
+//                e.printStackTrace()
+//                Toast.makeText(this, "Error getting address", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//        return address
+        try {
+            val addresses = geocoder.getFromLocation(
+                llng.latitude,
+                llng.longitude,
+                1
+            )
+            if (addresses != null) {
+                if (addresses.isNotEmpty()) {
+                    address = addresses[0]?.getAddressLine(0)
+                    // Use the obtained address
+                    Toast.makeText(this, address, Toast.LENGTH_SHORT).show()
+                } else {
+                    // No address found
+                    Toast.makeText(this, "No address found for the location", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Error getting address", Toast.LENGTH_SHORT).show()
+        }
+        return address
     }
 
 }
